ce6bbe00b65577957ec6430338a3b2e4851676ca
Mohammed Ali <mohaali482@gmail.com>
2023-04-01 11:19:55 +0300

    Added nasheed model

--- nasheeds/models.py
+++ nasheeds/models.py
@@ -1,3 +1,28 @@
+from audio_validator.validator import AudioValidator
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Nasheed(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("Owner"), on_delete=models.CASCADE)
+    name = models.CharField(_("Name"), max_length=255)
+    poster = models.ImageField(_("Poster"), upload_to="nasheeds/posters")
+    audio = models.FileField(
+        _("Audio"),
+        upload_to="nasheeds/audios",
+        validators=[AudioValidator("mp3")]
+    )
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+
+    class Meta:
+        verbose_name = _("nasheed")
+        verbose_name_plural = _("nasheeds")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("nasheed", kwargs={"pk": self.pk})
