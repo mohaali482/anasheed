8d84ee7153d35f93f391db145245b6456de0e7b8
Mohammed Ali <mohaali482@gmail.com>
2023-04-03 08:33:18 +0300

    Added a model for user images

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Image(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, verbose_name=_("User"), on_delete=models.CASCADE
+    )
+    image = models.ImageField(
+        _("Image"), upload_to="users/images", blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = _("userimage")
+        verbose_name_plural = _("userimages")
+
+    def __str__(self):
+        return self.user.username
