ef48c3dc8bd5a6b39d9848d47738dd45c9ea9d14
Mohammed Ali <mohaali482@gmail.com>
2023-04-03 09:36:34 +0300

    Added urls for authentication views

---
+++ authentication/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from .views import (
+    ChangePasswordAPIView,
+    CookieTokenObtainPairView,
+    CookieTokenRefreshView,
+    UserRetrieveUpdateAPIView,
+    UserViewSet,
+)
+
+router = DefaultRouter()
+router.register("users", UserViewSet)
+
+urlpatterns = [
+    path("token/", CookieTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", CookieTokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "me", UserRetrieveUpdateAPIView.as_view(), name="user_retrieve_update_api_view"
+    ),
+    path("me/change-password", ChangePasswordAPIView.as_view(), name="change-password"),
+] + router.urls
