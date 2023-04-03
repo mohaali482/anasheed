53185ef7bdfde93d16d77859da8c15bf37021973
Mohammed Ali <mohaali482@gmail.com>
2023-04-03 09:50:58 +0300

    Added nasheed owner permission

---
+++ nasheeds/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+
+class NasheedOwnerPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        if user == obj.owner:
+            return True
+
+        return False
