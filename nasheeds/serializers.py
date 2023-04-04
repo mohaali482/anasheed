fddf7ddc2e38d0935e5f0fabc570387310df1c49
Mohammed Ali <mohaali482@gmail.com>
2023-04-04 11:39:48 +0300

    Change field to a read only field

--- nasheeds/serializers.py
+++ nasheeds/serializers.py
@@ -7,3 +7,9 @@ class NasheedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Nasheed
         fields = "__all__"
+        read_only_fields = ["owner"]
+
+    def create(self, validated_data):
+        owner = self.context["request"].user
+        validated_data["owner"] = owner
+        return super().create(validated_data)
