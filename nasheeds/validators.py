cac468db9ed782516d1df9703449bb4957dea4c9
Mohammed Ali <mohaali482@gmail.com>
2023-04-04 11:05:26 +0300

    Added more constraint for the validator

--- nasheeds/validators.py
+++ nasheeds/validators.py
@@ -1,8 +1,15 @@
+import os
+
 from django.core.exceptions import ValidationError
 
 ALLOWED_AUDIO_TYPES = [
     "audio/mpeg",
     "audio/wav",
+]
+
+ALLOWED_EXTENSIONS = [
+    ".mp3",
+    ".wav",
 ]
 
 
@@ -11,3 +18,7 @@ def validate_audio(file):
         raise ValidationError("Audio size too large. Must be less than 10MB.")
     if file.content_type not in ALLOWED_AUDIO_TYPES:
         raise ValidationError("File type not supported.")
+
+    _, file_extension = os.path.splitext(file.name)
+    if file_extension.lower() not in ALLOWED_EXTENSIONS:
+        raise ValidationError("File extension not supported.")
