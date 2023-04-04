7106c21b2732d31b7f437f3a62b684bc607df8c9
Mohammed Ali <mohaali482@gmail.com>
2023-04-04 11:40:44 +0300

    Added admin serializer

--- nasheeds/serializers.py
+++ nasheeds/serializers.py
@@ -13,3 +13,9 @@ class NasheedSerializer(serializers.ModelSerializer):
         owner = self.context["request"].user
         validated_data["owner"] = owner
         return super().create(validated_data)
+
+
+class AdminNasheedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Nasheed
+        fields = "__all__"
