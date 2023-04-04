4689334aa9b3fe9d52951c2537f97a2a15d58108
Mohammed Ali <mohaali482@gmail.com>
2023-04-04 10:44:37 +0300

    Changed audio validation for the models

--- nasheeds/models.py
+++ nasheeds/models.py
@@ -1,18 +1,20 @@
-from audio_validator.validator import AudioValidator
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from .validators import validate_audio
 
 
 class Nasheed(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("Owner"), on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, verbose_name=_("Owner"), on_delete=models.CASCADE
+    )
     name = models.CharField(_("Name"), max_length=255)
     poster = models.ImageField(_("Poster"), upload_to="nasheeds/posters")
     audio = models.FileField(
         _("Audio"),
         upload_to="nasheeds/audios",
-        validators=[AudioValidator("mp3")]
+        validators=[validate_audio],
     )
     created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
