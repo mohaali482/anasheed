9517decf035088a483bb3cdc3258a38b5ab8722d
Mohammed Ali <mohaali482@gmail.com>
2023-04-22 15:05:55 +0300

    Changed serializer to contain more information

--- nasheeds/serializers.py
+++ nasheeds/serializers.py
@@ -1,21 +1,41 @@
+from authentication.serializers import RegularUserSerializer
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Nasheed
 
+User = get_user_model()
+
 
 class NasheedSerializer(serializers.ModelSerializer):
+    owner = RegularUserSerializer(read_only=True)
+
     class Meta:
         model = Nasheed
         fields = "__all__"
-        read_only_fields = ["owner"]
+        # read_only_fields = ["owner"]
 
     def create(self, validated_data):
-        owner = self.context["request"].user
+        # owner = self.context["request"].user
+        from django.contrib.auth import get_user_model
+
+        user = get_user_model()
+        owner = user.objects.all().first()
         validated_data["owner"] = owner
         return super().create(validated_data)
 
 
 class AdminNasheedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Nasheed
+        fields = "__all__"
+
+    def to_representation(self, instance):
+        self.fields["owner"] = RegularUserSerializer(read_only=True)
+        return super().to_representation(instance)
+
+
+class AdminUpdateNasheedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Nasheed
         fields = "__all__"
--- nasheeds/views.py
+++ nasheeds/views.py
@@ -2,16 +2,22 @@ from rest_framework import permissions, viewsets
 
 from .models import Nasheed
 from .permissions import NasheedPermissions
-from .serializers import NasheedSerializer, AdminNasheedSerializer
+from .serializers import (
+    AdminNasheedSerializer,
+    AdminUpdateNasheedSerializer,
+    NasheedSerializer,
+)
 
 
 class NasheedModelViewSet(viewsets.ModelViewSet):
     serializer_class = NasheedSerializer
     queryset = Nasheed.objects.all()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, NasheedPermissions]
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly, NasheedPermissions]
 
     def get_serializer_class(self):
         if self.request.user.is_staff:
+            if self.action == "update":
+                return AdminUpdateNasheedSerializer
             return AdminNasheedSerializer
         return super().get_serializer_class()
 
