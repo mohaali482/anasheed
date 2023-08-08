d0083ad14ffb799dab4d32e60c113e5fec5ddab0
Mohammed Ali <mohaali482@gmail.com>
2023-08-08 10:54:49 +0300

    Map saved nasheeds model viewset with a url

--- nasheeds/urls.py
+++ nasheeds/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import NasheedModelViewSet
+from .views import NasheedModelViewSet, SavedNasheedModelViewSet
 
 router = DefaultRouter()
 router.register("nasheeds", NasheedModelViewSet)
+router.register("saved-nasheeds", SavedNasheedModelViewSet, basename="saved-nasheeds")
 urlpatterns = [] + router.urls
