8eaa726bd3d6696bbd9dd7afe023fa809c68768c
Mohammed Ali <mohaali482@gmail.com>
2023-08-08 10:53:52 +0300

    Add filter for saved nasheeds

--- nasheeds/filters.py
+++ nasheeds/filters.py
@@ -1,9 +1,15 @@
 from django_filters import rest_framework as filters
 
-from .models import Nasheed
+from .models import Nasheed, SavedNasheed
 
 
 class NasheedsFilter(filters.FilterSet):
     class Meta:
         model = Nasheed
         fields = {"owner": ("exact",), "name": ("contains",)}
+
+
+class SavedNasheedsFilter(filters.FilterSet):
+    class Meta:
+        model = SavedNasheed
+        fields = {"nasheed__name": ("contains",)}
