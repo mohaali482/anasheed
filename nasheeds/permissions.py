1a74c0e578da006c3be2d294e9f9a16ba0fcf613
Mohammed Ali <mohaali482@gmail.com>
2023-08-15 21:31:40 +0300

    Add safe methods to allow read only

--- nasheeds/permissions.py
+++ nasheeds/permissions.py
@@ -3,6 +3,9 @@ from rest_framework import permissions
 
 class NasheedPermissions(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
         user = request.user
         if user == obj.owner:
             return True
