78684d2f8cc7d7878a296151386690190e87f75e
Mohammed Ali <mohaali482@gmail.com>
2023-12-08 17:44:56 +0300

    Remove trailing slashes on modelviewsets

--- nasheeds/urls.py
+++ nasheeds/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import MyNasheedModelViewSet, NasheedModelViewSet, SavedNasheedModelViewSet
 
 router = DefaultRouter()
-router.register("nasheeds/", NasheedModelViewSet)
-router.register("my-nasheeds/", MyNasheedModelViewSet, basename="my-nasheeds")
-router.register("saved-nasheeds/", SavedNasheedModelViewSet, basename="saved-nasheeds")
+router.register("nasheeds", NasheedModelViewSet)
+router.register("my-nasheeds", MyNasheedModelViewSet, basename="my-nasheeds")
+router.register("saved-nasheeds", SavedNasheedModelViewSet, basename="saved-nasheeds")
 urlpatterns = [] + router.urls
