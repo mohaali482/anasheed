88d2b202c627c98f91ec5acbb744e96461b7ef2d
Mohammed Ali <mohaali482@gmail.com>
2023-12-08 17:36:45 +0300

    Add slashes to endpoints

--- authentication/urls.py
+++ authentication/urls.py
@@ -17,23 +17,25 @@ router = DefaultRouter()
 router.register("users", UserViewSet)
 
 urlpatterns = [
-    path("token", CookieTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh", CookieTokenRefreshView.as_view(), name="token_refresh"),
+    path("token/", CookieTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", CookieTokenRefreshView.as_view(), name="token_refresh"),
     path(
         "me",
         UserRetrieveUpdateDestroyAPIView.as_view(),
         name="user_retrieve_update_api_view",
     ),
     path(
-        "me/delete-image",
+        "me/delete-image/",
         UserProfileDestroyAPIView.as_view(),
         name="user_profile_destroy_api_view",
     ),
-    path("me/change-password", ChangePasswordAPIView.as_view(), name="change-password"),
-    path("signup", UserSignup.as_view(), name="user_signup"),
-    path("forgot-password", ForgotPasswordAPIView.as_view(), name="forgot-password"),
     path(
-        "reset-password/<slug:uid>/<slug:token>",
+        "me/change-password/", ChangePasswordAPIView.as_view(), name="change-password"
+    ),
+    path("signup/", UserSignup.as_view(), name="user_signup"),
+    path("forgot-password/", ForgotPasswordAPIView.as_view(), name="forgot-password"),
+    path(
+        "reset-password/<slug:uid>/<slug:token>/",
         PasswordResetAPIView.as_view(),
         name="reset-password",
     ),
