b42c89f2f2270a0181db73b028ed85f8db5b2592
Mohammed Ali <mohaali482@gmail.com>
2023-12-09 16:10:41 +0300

    Removed bugs

--- nasheeds/serializers.py
+++ nasheeds/serializers.py
@@ -33,14 +33,10 @@ class NasheedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Nasheed
         fields = "__all__"
-        # read_only_fields = ["owner"]
+        read_only_fields = ["owner"]
 
     def create(self, validated_data):
-        # owner = self.context["request"].user
-        from django.contrib.auth import get_user_model
-
-        user = get_user_model()
-        owner = user.objects.all().first()
+        owner = self.context["request"].user
         validated_data["owner"] = owner
         return super().create(validated_data)
 
